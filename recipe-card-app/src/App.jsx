8a468abb22264c5430dcd4d2f5a72c76576cfac9
Agraz248 <185479959+Agraz-10@users.noreply.github.com>
2026-07-12 23:10:31 +0545

    feat: render recipe cards using props and map

--- recipe-card-app/src/App.jsx
+++ recipe-card-app/src/App.jsx
@@ -1,9 +1,28 @@
 import RecipeCard from "./components/RecipeCard";
+import recipes from "./data/recipes";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center items-center p-8">
-      <RecipeCard />
+    <div className="min-h-screen bg-gray-100 p-8">
+
+      <h1 className="text-4xl font-bold text-center mb-10">
+        🍽️ Recipe Book
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+        {recipes.map((recipe) => (
+          <RecipeCard
+            key={recipe.id}
+            image={recipe.image}
+            name={recipe.name}
+            time={recipe.time}
+            difficulty={recipe.difficulty}
+            ingredients={recipe.ingredients}
+          />
+        ))}
+
+      </div>
     </div>
   );
 }
--- recipe-card-app/src/components/RecipeCard.jsx
+++ recipe-card-app/src/components/RecipeCard.jsx
@@ -1,23 +1,23 @@
-function RecipeCard() {
+function RecipeCard(props) {
   return (
     <div className="max-w-sm bg-white rounded-xl shadow-lg overflow-hidden hover:scale-105 transition duration-300">
       <img
-        src="https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?w=800"
-        alt="Creamy Pasta"
+        src={props.image}
+        alt={props.name}
         className="w-full h-48 object-cover"
       />
 
       <div className="p-5">
         <h2 className="text-2xl font-bold mb-2">
-          Creamy Pasta
+          {props.name}
         </h2>
 
         <p className="text-gray-600 mb-2">
-          ⏱ Cooking Time: 25 mins
+          ⏱ Cooking Time: {props.time}
         </p>
 
         <p className="text-green-600 font-semibold mb-3">
-          ⭐ Easy
+          ⭐ {props.difficulty}
         </p>
 
         <h3 className="font-semibold mb-2">
@@ -25,10 +25,9 @@ function RecipeCard() {
         </h3>
 
         <ul className="list-disc ml-5 text-gray-700">
-          <li>Pasta</li>
-          <li>Fresh Cream</li>
-          <li>Garlic</li>
-          <li>Parmesan Cheese</li>
+            {props.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+            ))}
         </ul>
       </div>
     </div>
